c7ea0a263319aecd724b9174a9e5737a14678ed2
bhuwanchandrapant <pantbhuwan687@gmail.com>
2023-12-18 02:22:12 +0530

    example of arrays

---
+++ Arrays.java
@@ -0,0 +1,23 @@
+public class Arrays {
+    public static void main(String[] args) {
+         // 1st type
+        /*int [] marks;  //declaration
+        marks = new int[5]; // memory allocation
+
+          // 2nd type
+        int [] marks = new int [5];  // declaration + memory allocation
+        marks [0] = 12;
+        marks [1] = 22;
+        marks [2] = 32;
+        marks [3] = 42;
+        marks [4] = 52;
+        System.out.println( marks[2]);
+        System.out.println(marks[1]);
+        //3rd type
+        int[] marks = {12,22,32,42,52}; // declaration + initialize
+        System.out.println(marks[3]);  */
+      
+        
+
+    }
+}
---
+++ array2.java
@@ -0,0 +1,40 @@
+public class array2 {
+    public static void main(String[] args) {
+        /*int [] marks = {12,34,56,78,90};
+        System.out.println(marks.length);
+        System.out.println(marks[3]);
+
+        float[] number = {12.3f,23.5f,56.7f,78.9f};
+        System.out.println(number.length);
+        System.out.println(number[3]);
+
+
+        String[] name = {"bhuwan","chandra","pant"};
+        System.out.println(name.length);
+        System.out.println(name[2]);  */
+
+
+          //displaying the array (for loop)
+
+          System.out.println("for loop example");
+          int [] marks = {23,44,55,66,77};
+
+          for(int i = 0;i<marks.length;i++){
+            System.out.println(marks[i]);
+          }
+
+           // quick quig---displaying the array(for loop) in reverse
+           System.out.println("in reverse order");
+           for(int i = marks.length-1;i>=0;i--){
+            System.out.println(marks[i]);
+           }
+
+         //////displaying the array ( for-each loop)
+         System.out.println("for-each loop example");
+         for (int element: marks){
+            System.out.println(element);
+         }
+
+
+    }
+}
