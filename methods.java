decae0dd8d974ced64351edd08cdb174839c81a4
bhuwanchandrapant <pantbhuwan687@gmail.com>
2023-12-19 03:03:09 +0530

    example of different methods

--- methods.java
+++ methods.java
@@ -1,6 +1,98 @@
 public class methods {
+
+      static int logic(int x, int y){        //example of method
+
+            int z;
+            if (x>y) {
+                z = x+y;
+            }
+            else{
+                z = (x+y) * 5;
+            }
+            return z;
+
+      }
     public static void main(String[] args) {
         int a = 5;
+        int b = 7;
+        int c;
+        c = logic(a, b);
+        int a1 = 2;
+        int b1 = 1;
+        int c1;
+        if (a1>b1) {
+            c1 = a1+b1;
+            
+        }
+        else{
+            c1 = (a1+b1) * 5;
+
+        }
+        System.out.println(c);
+        System.out.println(c1);
+
+     
+         
+         
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /*  int a = 5;
         int b = 7;
         int c;
         if (a>b) {
@@ -23,5 +115,6 @@ public class methods {
 
         }
         System.out.println(c);
+        System.out.println(c1);*/
     }
 }
