193e32fd1a2d68fca6df58f70ae3b33e47e68a52
bhuwanchandrapant <pantbhuwan687@gmail.com>
2023-12-19 03:22:33 +0530

    example of methods

--- methods.java
+++ methods.java
@@ -1,7 +1,7 @@
 public class methods {
 
       static int logic(int x, int y){        //example of method
-
+      // int logic(int x , int y)--> method invocation using object creation
             int z;
             if (x>y) {
                 z = x+y;
@@ -16,18 +16,16 @@ public class methods {
         int a = 5;
         int b = 7;
         int c;
+        //((((method invocation using object creation)))
+        //methods obj = new methods();
+        //c = obj.logic(a,b);
         c = logic(a, b);
         int a1 = 2;
         int b1 = 1;
         int c1;
-        if (a1>b1) {
-            c1 = a1+b1;
-            
-        }
-        else{
-            c1 = (a1+b1) * 5;
-
-        }
+        c1 = logic(a1, b1 );
+        // c1 = obj.logic(a1,b1);
+        
         System.out.println(c);
         System.out.println(c1);
 
