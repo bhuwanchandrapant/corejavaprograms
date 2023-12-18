555d34574f6ffa781b4cec83397331b425d1da0d
bhuwanchandrapant <pantbhuwan687@gmail.com>
2023-12-19 02:17:59 +0530

    examples of methods

---
+++ methods.java
@@ -0,0 +1,27 @@
+public class methods {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c;
+        if (a>b) {
+            c = a+b;
+
+    
+        }
+        else{
+            c = (a+b) * 5;
+        }
+        int a1 = 2;
+        int b1 = 1;
+        int c1;
+        if (a1>b1) {
+            c1 = a1+b1;
+            
+        }
+        else{
+            c1 = (a1+b1) * 5;
+
+        }
+        System.out.println(c);
+    }
+}
