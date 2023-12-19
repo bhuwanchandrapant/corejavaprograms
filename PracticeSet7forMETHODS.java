d95a085756cc04e0c6b6a059b924a179cc2f0c46
bhuwanchandrapant <pantbhuwan687@gmail.com>
2023-12-20 03:30:19 +0530

    pracice question of array methods and recursion

---
+++ PracticeSet7forMETHODS.java
@@ -0,0 +1,33 @@
+public class PracticeSet7forMETHODS {
+
+
+
+static void multification(int n){       /////question1
+    for(int i=1;i<=10;i++){
+        System.out.format("%d X %d = %d\n",n, i, n*i);
+
+    }
+}
+ static void pattern(int n){      //question 2
+    for (int i=0;i<=n;i++){
+        for(int j=0;j<i+1;j++){
+            System.out.print(" * ");
+
+        }
+        System.out.println("");
+    }
+ }
+
+
+
+    public static void main(String[] args) {
+        ////question1
+    
+        multification(8);
+
+        ////question2
+        pattern(4);
+        
+        
+    }
+}
