3c2e800a049f6df76de2314c7c894bcf0ff84173
bhuwanchandrapant <pantbhuwan687@gmail.com>
2023-12-22 23:48:57 +0530

    star pattern program

---
+++ starpattern.java
@@ -0,0 +1,25 @@
+public class starpattern {
+    public static void main(String[] args) {
+       
+        for(int i=0;i<=3;i++){
+            
+            for(int j=i;j<=3;j++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+    
+    
+       
+        for(int i=0;i<=3;i++){
+            
+            for(int j =0;j<=3;j++){
+                System.out.print("*  ");
+            }System.out.println();
+        }
+    
+    
+    
+   
+    
+    }
+}
