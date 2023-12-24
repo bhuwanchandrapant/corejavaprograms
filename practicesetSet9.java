394bd44c328f926d5685bc1ca4a4665c824f0bda
bhuwanchandrapant <pantbhuwan687@gmail.com>
2023-12-25 03:19:46 +0530

    getterAndsetterAndConstructor programs

--- practicesetSet9.java
+++ practicesetSet9.java
@@ -2,6 +2,11 @@
 class cylinder{
     private int radius;
     private int height;
+    
+    public cylinder(int radius, int height) {
+        this.radius = radius;
+        this.height = height;
+    }
     public int getRadius() {
         return radius;
     }
@@ -14,17 +19,64 @@ class cylinder{
     public void setHeight(int height) {
         this.height = height;
     }
+    public double surfaceArea(){
+        return 2* Math.PI* radius * radius + 2*Math.PI*radius*height;
+    }
+    public double volume(){
+        return Math.PI* radius * radius * height; 
+    }
+
+}
+class rectangle{
+    private int length;
+    private int breadth;
+
+     public rectangle(){
+        this.length = 4;
+        this.breadth = 5;
+
+
+     }
+
+
+    public rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+
+    public int getLength() {
+        return length;
+    }
+
+
+    public int getBreadth() {
+        return breadth;
+    }
+        
 
 }
 
 
 public class practicesetSet9 {
     public static void main(String[] args) {
-        cylinder mCylinder = new cylinder();
-        mCylinder.setHeight(123);
+
+         ///question 1
+        cylinder mCylinder = new cylinder(9,12);
+        mCylinder.setHeight(12);
         System.out.println(mCylinder.getHeight());
-        mCylinder.setRadius(1234);
+        mCylinder.setRadius(9);
         System.out.println(mCylinder.getRadius());
+
+        ///question 2
+        System.out.println(mCylinder.surfaceArea());
+        System.out.println(mCylinder.volume());
+
+        //question 3
+        rectangle r = new rectangle();
+        System.out.println(r.getLength());
+        System.out.println(r.getBreadth());
+        
     }
     
 }
