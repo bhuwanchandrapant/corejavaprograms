8dd394522031c2b2a18fc7edf071e0b05d905bd3
bhuwanchandrapant <pantbhuwan687@gmail.com>
2024-01-01 21:21:34 +0530

    shortname

---
+++ shortname.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class shortname {
+      public static void main(String args[])
+{
+ 
+ 
+String S1="Hello";
+String S2="hello";
+System.out.println("First String = "+S1);
+System.out.println("Second String = "+S2);
+int x=S1.compareTo(S2);
+if(x==0)
+System.out.println("String are same");
+else
+System.out.println("String are not same");
+}
+}
