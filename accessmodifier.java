41bce400fd9ba1dbee775d7fedae4b2a68746947
bhuwanchandrapant <pantbhuwan687@gmail.com>
2024-01-04 23:25:10 +0530

    concepts of access modifier

---
+++ accessmodifier.java
@@ -0,0 +1,23 @@
+
+class c1 {
+    public int x =8;
+    protected int y = 10;
+    int z = 7;
+    private int a =17;
+    public void meth1(){
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+    
+    }  
+    }  
+
+public class accessmodifier {
+    public static void main(String[] args) {
+        c1 c =new c1();
+        c.meth1();
+
+        
+    }
+}
