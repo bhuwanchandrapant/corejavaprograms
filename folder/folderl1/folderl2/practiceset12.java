4de8fb28b84b43258c147f02e8f6eb311429abd3
bhuwanchandrapant <pantbhuwan687@gmail.com>
2024-01-07 21:01:50 +0530

    practice on packages

---
+++ folder/folderl1/folderl2/practiceset12.java
@@ -0,0 +1,25 @@
+package folder.folderl1.folderl2;
+
+class calculator{
+    public void calculate(int a, int b){
+        System.out.println("your result is "+ a+b);
+
+    }
+    class sccalculator{
+        public void calculate(int a, int b){
+            System.out.println("your result is "+ Math.sin(a+b));
+
+        }
+    }
+    }
+    class hycalculator{
+        public void calculate(int a, int b){
+            System.out.println("your result is "+ a+b);
+            System.out.println("your result is  "+ Math.sin(a+b));
+        }
+    }
+public class practiceset12 {
+    public static void main(String[] args) {
+        System.out.println("packages are foder in folder in folder  ek se jyada package");
+    }
+}
