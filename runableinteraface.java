e24c912f92c2b8ec2263f82ff51fd18f88804069
bhuwanchandrapant <pantbhuwan687@gmail.com>
2024-01-11 12:34:03 +0530

    Thread using runnable interface

---
+++ runableinteraface.java
@@ -0,0 +1,35 @@
+
+class MyThreadRunnable1 implements Runnable{
+    public void run(){
+        System.out.println("i am thread 1 not thred");
+        System.out.println("i am thread 1 not thred");
+        System.out.println("i am thread 1 not thred");
+        System.out.println("i am thread 1 not thred");
+        System.out.println("i am thread 1 not thred");
+        System.out.println("i am thread 1 not thred");
+        System.out.println("i am thread 1 not thred");
+    }
+}
+class MyThreadRunnable2 implements Runnable{
+    public void run(){
+        System.out.println("iam thread2 not thread");
+        System.out.println("iam thread2 not thread");
+        System.out.println("iam thread2 not thread");
+        System.out.println("iam thread2 not thread");
+        System.out.println("iam thread2 not thread");
+        System.out.println("iam thread2 not thread");
+        System.out.println("iam thread2 not thread");
+        
+    }
+}
+
+public class runableinteraface {
+    public static void main(String[] args) {
+        MyThreadRunnable1 bullet1 = new MyThreadRunnable1();
+        Thread gun1 = new Thread(bullet1);
+        MyThreadRunnable2 bullet2 = new MyThreadRunnable2();
+        Thread gun2 = new Thread(bullet2);
+        gun1.start();
+        gun2.start();
+    }
+}
