127909b16c8c87f3fd0c099ab0059bb4a6d10b54
bhuwanchandrapant <pantbhuwan687@gmail.com>
2024-01-29 13:32:05 +0530

    practice question on errors and exception

---
+++ practiceset14.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class practiceset14 {
+    public static void main(String[] args) {
+        ///question2
+        try{
+            int a =  666/0;
+        }
+        catch(IllegalArgumentException e){
+            System.out.println("hehe");
+        }
+        catch(ArithmeticException e ){
+            System.out.println("haha");
+
+        }
+
+
+        ////question3
+        boolean flag = true;
+        int [] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 17;
+        marks[2] = 27;
+        Scanner sc = new Scanner(System.in);
+        int index;
+        int i = 0;
+        System.out.println("enter the value of index");
+        while (flag && i<5) {
+            try{
+                index = sc.nextInt();
+            System.out.println("the value of [index] is " +marks[index]);
+            break;
+            }
+            catch(Exception e){
+                System.out.println("invalid index");
+                i++;
+            }
+            
+        }
+        if (i>=5) {
+            System.out.println("Errors");
+            
+        }
+
+    }
+}
