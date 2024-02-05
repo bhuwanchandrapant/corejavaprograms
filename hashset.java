8d880446b663165abb7a1b55237965f34a7a82da
bhuwanchandrapant <pantbhuwan687@gmail.com>
2024-02-05 15:12:11 +0530

    need of hashset in java

---
+++ hashset.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class hashset {
+    public static void main(String[] args) {
+        HashSet<Integer> myhashset = new HashSet<>(8, 0.5f);
+        myhashset.add(12);
+        myhashset.add(2);
+        myhashset.add(22);
+        myhashset.add(12);
+
+
+        System.out.println(myhashset);
+
+        
+    }
+}
