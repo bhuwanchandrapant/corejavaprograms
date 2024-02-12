4882210743c055bc988be23ded8b721631f10fd7
bhuwanchandrapant <pantbhuwan687@gmail.com>
2024-02-12 11:00:13 +0530

    date class

---
+++ date_class.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class date_class {
+    public static void main(String[] args) {
+        //System.out.println(Long.MAX_VALUE);
+        //System.out.println(System.currentTimeMillis());
+        Date d = new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+        System.out.println(d.getDate());
+        System.out.println(d.getHours());
+    }
+}
