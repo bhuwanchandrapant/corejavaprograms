87cee9d546f42c423a1b4eab3e5ae77223c0baca
bhuwanchandrapant <pantbhuwan687@gmail.com>
2024-02-24 09:16:59 +0530

    how to check window on java

---
+++ checkOS.java
@@ -0,0 +1,9 @@
+
+import java.io.File;
+@SuppressWarnings("unused")
+public class checkOS {
+    public static void main(String[] args) {
+        String OS = System.getProperty("os.name");
+        System.out.println(OS);
+    }
+}
